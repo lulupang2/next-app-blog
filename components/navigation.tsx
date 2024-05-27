79dcd034eb99cd8ceeded8ea2f0deae0801af65e
lulupang <lulupang@live.co.kr>
2024-05-28 03:09:49 +0900

    feat: :sparkles: add themeProvider

--- components/navigation.tsx
+++ components/navigation.tsx
@@ -1,13 +1,22 @@
-import Link from "next/link";
+import Link from 'next/link';
+import { Icons } from './ui/icon';
+import { ThemeToggle } from './theme-toggle';
 
 export default function Navigation() {
   return (
-    <header className="w-full sticky top-0 p-4 border-b-2 shadow bg-white z-50">
-      <div className="container flex items-center justify-between">
+    <header className="w-full  sticky top-0 z-10">
+      <div className="container p-4 flex items-center justify-between bg-white text-black dark:bg-black dark:text-white">
         <span>My Blog</span>
-        <nav className="text-sm font-medium space-x-6">
-          <Link href="/">Home</Link>
-          <Link href="/blog">Blog</Link>
+        <nav className="flex text-sm font-medium space-x-6">
+          <ThemeToggle />
+          <Link
+            href={'https://github.com/lulupang'}
+            target="_blank"
+            rel="noreferrer"
+          >
+            <Icons.gitHub className="h-7 w-7" />
+            <span className="sr-only">GitHub</span>
+          </Link>
         </nav>
       </div>
     </header>
