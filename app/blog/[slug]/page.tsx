f73f7f9b7bb7e81f8468bdbcdb2e0081c43a4466
lulupang <lulupang@live.co.kr>
2024-05-28 19:56:00 +0900

    feat: :sparkles: add metadata

--- app/blog/[slug]/page.tsx
+++ app/blog/[slug]/page.tsx
@@ -54,6 +54,28 @@ export default async function Page({ params }: { params: Params }) {
 
   return (
     <article className="relative container bg-white text-black dark:bg-black dark:text-white py-6 lg:gap-10 lg:py-8 xl:grid xl:grid-cols-[1fr_300px]">
+      <script
+        type="application/ld+json"
+        suppressHydrationWarning
+        dangerouslySetInnerHTML={{
+          __html: JSON.stringify({
+            '@context': 'https://schema.org',
+            '@type': 'BlogPosting',
+            headline: postData.frontmatter.title,
+            datePublished: postData.frontmatter.date,
+            dateModified: postData.frontmatter.date,
+            description: postData.frontmatter.description,
+            image: postData.frontmatter.thumbnail
+              ? `${BASE_URL}${postData.frontmatter.thumbnail}`
+              : `/og?title=${encodeURIComponent(postData.frontmatter.title)}`,
+            url: `${BASE_URL}/blog/${params.slug}`,
+            author: {
+              '@type': 'Person',
+              name: 'My Portfolio',
+            },
+          }),
+        }}
+      />
       <PostContents source={postData.source} />
       <div className="hidden text-sm xl:block">
         <div className="sticky top-16 -mt-10 pt-4">
